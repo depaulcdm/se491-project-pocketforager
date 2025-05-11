325f4eb444b7ed4174034df2ee1ed3a318bf0c46
Marina <mljuboja@luc.edu>
2025-05-11 18:17:39 -0500

    Updated Details Page Design

--- app/src/main/java/com/example/pocketforager/DetailsFragment.java
+++
@@ -1,7 +0,0 @@
-package com.example.pocketforager;
-
-import androidx.fragment.app.Fragment;
-
-public class DetailsFragment extends Fragment {
-    public DetailsFragment() { super(R.layout.fragment_details); }
-}
