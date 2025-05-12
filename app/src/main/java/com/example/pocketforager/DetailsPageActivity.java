7aeaf032b8ff30367ee79db40169423808910c06
Marina <mljuboja@luc.edu>
2025-05-11 19:59:52 -0500

    changed layout file

---
+++ app/src/main/java/com/example/pocketforager/DetailsPageActivity.java
@@ -0,0 +1,46 @@
+package com.example.pocketforager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.pocketforager.model.Plant;
+
+public class DetailsPageActivity extends AppCompatActivity {
+    public static final String EXTRA_PLANT = "extra_plant";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        ImageView ivPhoto = findViewById(R.id.imagePlantDetail);
+        TextView tvNoPhoto = findViewById(R.id.tvNoPhotoDetail);
+        TextView tvName = findViewById(R.id.tvPlantNameDetail);
+        TextView tvSci = findViewById(R.id.tvScientificNameDetail);
+        TextView tvOther = findViewById(R.id.tvOtherNameDetail);
+        TextView tvEdible = findViewById(R.id.tvEdibleDetail);
+
+        Intent intent = getIntent();
+        Plant plant = (Plant) intent.getSerializableExtra(EXTRA_PLANT);
+
+        if (plant != null) {
+            // Photo
+            if (plant.getImageUrl() != null && !plant.getImageUrl().isEmpty()) {
+                tvNoPhoto.setVisibility(View.GONE);
+
+            } else {
+                tvNoPhoto.setVisibility(View.VISIBLE);
+            }
+
+            tvName.setText(plant.getCommonName());
+            tvSci.setText(plant.getScientificName());
+            tvOther.setText(plant.getOtherName());
+            tvEdible.setText(plant.isEdible() ? "Yes" : "No");
+        }
+    }
+}
--- app/src/main/java/com/example/pocketforager/MainActivity.java
+++ app/src/main/java/com/example/pocketforager/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.pocketforager;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
