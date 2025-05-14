6eb9754b816c158847d25899366e166f75f62c61
atifsiddiqui20 <asiddi27@depaul.edu>
2025-05-14 15:07:53 -0500

    Added scrollview to details page

---
+++ app/src/test/java/com/example/pocketforager/MainActivityTest.java
@@ -0,0 +1,21 @@
+package com.example.pocketforager;
+
+import junit.framework.TestCase;
+
+public class MainActivityTest extends TestCase {
+
+    public void testOnCreate() {
+    }
+
+    public void testAcceptPlants() {
+    }
+
+    public void testSearch() {
+    }
+
+    public void testClearSearch() {
+    }
+
+    public void testOpenDetails() {
+    }
+}
